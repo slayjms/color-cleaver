158ec3167975eaebdbe5308104a6ed0f2fd641ba
Slay James <slayjms@yahoo.com>
2024-10-09 09:59:24 -0500

    four passing tests

--- color-combinator.js
+++ color-combinator.js
@@ -3,16 +3,23 @@
  ******************/
 // takes in two primary colors and gives the secondary made of those colors
 function colorCombinator(color1, color2){
-  if(color1 === 'red' + color2 === 'yellow'){
-    return 'makes orange!'
-  }else if(color1 === 'red' + color2 === 'blue'){
-    return 'makes purple!'
-  }else if(color1 === 'yellow' + color2 === 'blue'){
-    return 'makes green!'
+  if(color1 === 'red' && color2 === 'yellow'){
+    return 'orange'
+  }else if(color1 === 'red' && color2 === 'blue'){
+    return 'purple'
+  }else if(color1 === 'yellow' && color2 === 'blue'){
+    return 'green'
+  }else if(color1 === 'yellow' && color2 === 'red'){
+    return 'orange'
+  }else if(color1 === 'blue' && color2 === 'red'){
+    return 'purple'
+  }else if(color1 === 'blue' && color2 === 'yellow'){
+    return 'green'
   }else{
     return 'error'
   }
-}
+  }
+
 
 // Our setup code here. Don't touch!
 if(typeof colorCombinator === 'undefined') {
