ed4b9b47784c1e9ac13cc7b49efc9b592ee6b1b9
Slay James <slayjms@yahoo.com>
2024-10-09 10:29:37 -0500

    inputs

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,14 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+const color1 = getInput(1)
+
+
+
+
+// node color-cleaver.js blue yellow => console.log("Blue and yellow make green.")
+// node color-cleaver.js purple => console.log("Purple is made up of blue and red.")
+// node color-cleaver.js nothing => console.log("Must be valid primary and secondary colors.")
+// node color-cleaver.js blue blue => console.log("error")
+// node color-cleaver.js blue => console.log("Must be valid secondary")
